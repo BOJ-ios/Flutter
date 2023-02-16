a6e9b1a4808b064e6af3cb75a5e81a8a9c649130
DIANNA CADEN <44316764+BOJ-ios@users.noreply.github.com>
2023-02-17 04:31:20 +0900

    day2

--- toonflix/lib/main.dart
+++ toonflix/lib/main.dart
@@ -9,7 +9,10 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Text("Hello world"),
+        appBar: AppBar(
+          title: Text("Hello flutter"),
+        ),
+        body: Center(child: Text("Hello world")),
       ),
     );
   }
